88c26191ae660d2f62f3895752f74f29384a20cf
Komal Raza <komalraza258@gmail.com>
2024-08-11 08:19:48 +0500

    add loading-bar for posts

--- clients/src/component/posts/Posts.js
+++ clients/src/component/posts/Posts.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import Post from './Post/Post';
-import useStyles from './styles';
-import {useSelector} from 'react-redux';
-import {Grid, CircularProgress} from '@material-ui/core'
+import React from "react";
+import Post from "./Post/Post";
+import useStyles from "./styles";
+import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
-const Posts = ({setCurrentId}) => {
+const Posts = ({ setCurrentId }) => {
+  const { posts } = useSelector((state) => state.posts); //in that hook we access to whole global state/store
 
-  const {posts} = useSelector((state) => state.posts);  //in that hook we access to whole global state/store 
-   
-  
   const classes = useStyles();
-    //initialize it as hook
+  //initialize it as hook
 
-    return(
-      !posts?.length  ?<h2 style={{display:"flex",justifyContent:"center",alignItems:"center"}}> No Post Found</h2> : (
-       <Grid className={classes.container} container alignItems="stretch" spacing={3} >
-       {
-         posts?.map((postelm) => (
-           <Grid key={postelm._id} item xs={12} sm={12} md={6} lg={4} >
-            <Post post={postelm} setCurrentId={setCurrentId}/>
-           </Grid>
-         ))}
-       </Grid>
-    )
-  )
+  return !posts?.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts?.map((postelm) => (
+        <Grid key={postelm._id} item xs={12} sm={12} md={6} lg={4}>
+          <Post post={postelm} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default Posts;
